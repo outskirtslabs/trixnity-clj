e1cf3a0747c1ecc61e7dcc68a62b22b2f0c72f72
Casey Link <casey@outskirtslabs.com>
2026-03-25 09:25:35 +0100

    try latest n messages pattern

--- dev/user.clj
+++ dev/user.clj
@@ -1,7 +1,10 @@
 (ns user
   (:require
    #_[portal.api :as p]
-   [clj-reload.core :as clj-reload]))
+   [clj-reload.core :as clj-reload]
+   [missionary.core :as m]
+   [ol.trixnity.room :as room]
+   [ol.trixnity.schemas :as mx]))
 
 ((requiring-resolve 'hashp.install/install!))
 
@@ -11,6 +14,31 @@
 (clj-reload/init {:dirs      ["src" "dev" "test"]
                   :no-reload #{'user 'dev 'ol.dev.portal}})
 
+(defn- collect-values [flow n]
+  (m/? (->> flow
+            (m/eduction (take n))
+            (m/reduce conj []))))
+
+(defn latest-n-room-messages
+  "Returns the latest `n` normalized timeline events for `room-id` in
+  chronological order."
+  ([client room-id n]
+   (latest-n-room-messages client room-id n
+                           (java.time.Duration/ofSeconds 5)))
+  ([client room-id n fetch-timeout]
+   (let [message-flows (first (collect-values
+                               (room/get-last-timeline-events-list
+                                client
+                                room-id
+                                n
+                                1
+                                {::mx/fetch-timeout fetch-timeout
+                                 ::mx/fetch-size    n})
+                               1))]
+     (->> message-flows
+          (mapv #(first (collect-values % 1)))
+          reverse))))
+
 (comment
   (defonce ps ((requiring-resolve 'ol.dev.portal/open-portals)))
 
@@ -18,5 +46,7 @@
   (clj-reload/reload {:only :all}) ;; rcf
   #_(reset! my-portal/portal-state nil)
   ;;(clojure.repl.deps/sync-deps)
+  ;; Example:
+  ;; (latest-n-room-messages client "!room:example.org" 20)
   ;;;
   )
